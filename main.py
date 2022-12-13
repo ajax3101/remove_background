c3b34418b9bf14f829b1847756b4e5376e5d41d9
Denys Morozov <ajax3101@gmail.com>
2022-12-13 22:42:39 +0200

    u2net

--- main.py
+++ main.py
@@ -11,13 +11,17 @@ def remove_bg():
         all_files.extend(Path('.\input_imgs').glob(ext))
 
     #print(all_files)
-    for item in all_files:
+    for index, item in enumerate(all_files) :
         input_path = Path(item)
         file_name = input_path.stem
 
-        output_path = f'{file_name}_output.png'
-        print(output_path)
+        output_path = f'.\output_imgs\{file_name}_output.png'
+        #print(output_path)
+        input_img = Image.open(input_path)
+        output_img = remove(input_img)
+        output_img.save(output_path)
 
+        print(f'Completed: {index + 1}/{len(all_files)}')
 
 def main():
     remove_bg()
